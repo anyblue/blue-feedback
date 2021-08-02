af777f5d0fa230cddbc43c33ecaff520a8ecc590
Atlas-lili <3163299800@qq.com>
2021-08-02 14:59:10 +0800

    fix: throw less toast in textarea

--- src/textarea.ts
+++ src/textarea.ts
@@ -28,8 +28,15 @@ export default class Textarea extends EventCleaner {
         if (!this.textarea || !this.maxLength) {
             return;
         }
-        this.addEventListener(this.textarea, 'change', this.changeHanler);
-        this.addEventListener(this.textarea, 'input', this.changeHanler);
+        this.addEventListener(this.textarea, 'change', () => {
+            if (this.overflow) {
+                this.emiterror(new OverflowError('超过字数限制'));
+            }
+        });
+        this.addEventListener(this.textarea, 'input', (e: Event|{target?: HTMLInputElement}) => {
+            const length = e.target && 'value' in e.target ? e.target.value.length : 0;
+            this.updateValue(length);
+        });
     }
     get value() {
         return this.textarea?.value ?? '';
@@ -49,10 +56,6 @@ export default class Textarea extends EventCleaner {
     private hintContent(value: number): string {
         return `${value} / ${this.maxLength}`;
     }
-    private readonly changeHanler = (e: Event|{target?: HTMLInputElement}): void => {
-        const length = e.target && 'value' in e.target ? e.target.value.length : 0;
-        this.updateValue(length);
-    };
     private updateValue(length: number): void {
         if (this.hint) {
             this.hint.innerText = this.hintContent(length);
@@ -60,7 +63,6 @@ export default class Textarea extends EventCleaner {
         const elClassList = this.el.className.split(' ').map(item => item.trim());
         const errorIndex = elClassList.indexOf(styles.error);
         this.overflow = length > (this.maxLength ?? 0) && !!this.maxLength;
-        this.overflow && this.emiterror(new OverflowError('超过字数限制'));
         this.changeCallback?.(this.overflow ? new OverflowError('超过字数限制') : null, length);
         if (errorIndex !== -1 && !this.overflow) {
             elClassList.splice(errorIndex, 1);
